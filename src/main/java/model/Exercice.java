fcf79695a4c68959533efdd0cb980c3125a190de
ArnaudW31 <arnaudwq@gmail.com>
2025-11-14 10:38:49 +0100

    feat: plus catego -> muscles

--- src/main/java/model/Exercice.java
+++ src/main/java/model/Exercice.java
@@ -1,11 +1,12 @@
 package model;
 
 import javafx.scene.Scene;
+import java.util.List;
 
 public abstract class Exercice {
     private int id;
     private String nom;
-    private String categorie;
+    private List<Muscle> muscles;
     private String description;
 
     // public Exercice() {}
@@ -23,8 +24,8 @@ public abstract class Exercice {
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
 
-    public String getCategorie() { return categorie; }
-    public void setCategorie(String categorie) { this.categorie = categorie; }
+    public List<Muscle> getMuscles() { return muscles; }
+    public void setMuscles(List<Muscle> nvMuscles) { this.muscles = nvMuscles; }
 
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
@@ -32,7 +33,7 @@ public abstract class Exercice {
     @Override
     public String toString() {
         // utilisé pour ListView
-        return nom + (categorie != null && !categorie.isEmpty() ? " (" + categorie + ")" : "");
+        return nom;
     }
 
     public abstract Scene showExplanation();
