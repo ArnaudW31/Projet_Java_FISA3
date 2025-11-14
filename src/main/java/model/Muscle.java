6b2443547ddbf2832ec76ceb3957a18b68380051
ArnaudW31 <arnaudwq@gmail.com>
2025-11-14 10:48:44 +0100

    feat: contructeur muscle

--- src/main/java/model/Muscle.java
+++ src/main/java/model/Muscle.java
@@ -5,6 +5,11 @@ import javafx.scene.image.Image;
 public class Muscle {
     private String nom;
     private Image illustrationImage;
+
+    public Muscle(String nom, String imagePath){
+        this.nom = nom;
+        this.illustrationImage = new Image(imagePath);
+    }
     
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
