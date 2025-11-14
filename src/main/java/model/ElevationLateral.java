7d1cfe1e15525fb06751c8fba9351cdeacd38519
Jules0809 <146353092+Jules0809@users.noreply.github.com>
2025-11-14 10:23:22 +0100

    up

--- src/main/java/model/ElevationLateral.java
+++ src/main/java/model/ElevationLateral.java
@@ -1,5 +1,8 @@
 package model;
 
 public class ElevationLateral extends Exercice {
+    public ElevationLateral(int id, String nom, String categorie, String description) {
+        super(id, nom, categorie, description);
+    }
     
 }
